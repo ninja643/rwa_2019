ce0739600a401ef1f45778b58c21b7b03a80b04f
Marko Milosevic <ninja643@gmail.com>
2019-04-09 00:37:54 +0200

    Update order of test dependencies so that JMockit works

--- src/test/java/rs/ac/ni/pmf/web/model/converter/StudentConverterTest.java
+++ src/test/java/rs/ac/ni/pmf/web/model/converter/StudentConverterTest.java
@@ -2,20 +2,38 @@ package rs.ac.ni.pmf.web.model.converter;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import mockit.Injectable;
+import mockit.Tested;
 import org.junit.Test;
 
 import rs.ac.ni.pmf.web.model.api.StudentDTO;
 import rs.ac.ni.pmf.web.model.data.StudentEntity;
+import rs.ac.ni.pmf.web.repository.InfoRepository;
 
-public class StudentConverterTest {
+import java.util.Collections;
 
-	private StudentConverter converter = new StudentConverter();
+public class StudentConverterTest
+{
+
+	@Injectable
+	private InfoRepository infoRepository;
+
+	@Tested
+	private StudentConverter converter;
 
 	@Test
-	public void shouldConvertEntityToDto() {
+	public void shouldConvertEntityToDto()
+	{
 		final StudentEntity entity = StudentEntity.builder().id(1).firstName("Pera").lastName("Petrovic")
 				.studentId("1234-RN").build();
-		final StudentDTO expected = StudentDTO.builder().id(1).firstName("Pera").lastName("Petrovic").build();
+		final StudentDTO expected = StudentDTO.builder()
+				.id(1)
+				.firstName("Pera")
+				.lastName("Petrovic")
+				.studentId("1234-RN")
+				.emails(Collections.emptyList())
+				.phones(Collections.emptyList())
+				.build();
 
 		final StudentDTO actual = converter.fromEntity(entity);
 
